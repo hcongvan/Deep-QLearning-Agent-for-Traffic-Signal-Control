6e73b1927eb4c1a31d1eedd7300c2512461c064b
hcongvan <nfs28794@gmail.com>
2019-12-29 15:50:01 +0700

    start train 1

---
+++ TLCS/test_image.py
@@ -0,0 +1,26 @@
+import traci
+import numpy as np
+import random
+import sys,os
+from sumolib import checkBinary
+import TrafficGenerator
+from TrafficGenerator import TrafficGenerator
+# sumo things - we need to import python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+
+sumoBinary = checkBinary('sumo-gui')
+sumoCmd = [sumoBinary, "-c", "intersection/tlcs_config_train.sumocfg", "--no-step-log", "true", "--waiting-time-memory", str(100)]
+traci.start(sumoCmd)
+durations = 50
+for i in range(durations):
+    traci.simulationStep()
+    if i % 10 == 0:
+        traci.gui.screenshot('View #0','image/{}.png'.format(str(i)))
+
+gen = TrafficGenerator(5400)
+gen.generate_routefile(20)
